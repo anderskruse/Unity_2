d742bd02039b125bb99f8d1144a023de450ff505
Anderskruse <32634165+anderskruse@users.noreply.github.com>
2019-02-07 12:53:35 +0100

    FlightSim now done

---
+++ Assignment_2/Assets/PowerUpCounter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PowerUpCounter : MonoBehaviour
+
+{
+    [SerializeField] TextMeshProUGUI counter;
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void counterText()
+    {
+        counter.text = "Score: " + score.ToString();
+    }
+
+   public void increment()
+    {
+        score++;
+        counterText();
+    }
+}
---
+++ Assignment_2/Assets/PowerUps.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PowerUps : MonoBehaviour
+{
+    public AudioSource audio;
+    public GameObject player;
+    public PowerUpCounter pou;
+
+
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        audio.Play();
+        Destroy(gameObject);
+
+        if (other.gameObject.tag == "Player")
+        {
+            pou.increment();
+            
+            
+        }
+
+    }
+}
