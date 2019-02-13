9fd76fffeb3c3abdf6e828aa6635d71da0d44607
Anderskruse <32634165+anderskruse@users.noreply.github.com>
2019-02-13 09:45:34 +0100

    Green now fully done

---
+++ Assignment_2/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public GameObject explosion;
+    private GameObject expl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.gameObject.transform.tag == "Enemy")
+        {
+            expl = Instantiate(explosion);
+            expl.transform.position = other.transform.position;
+            expl.gameObject.SetActive(true);
+            other.gameObject.SetActive(false);
+        }
+        
+    }
+}
